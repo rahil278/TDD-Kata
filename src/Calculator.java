b92ca5d70252cac79870dcd58e2edd1329baa1d1
rahil <rahil.shah@visiblealpha.com>
2021-12-25 21:45:18 +0530

    testCase4

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,14 @@
 public class Calculator {
     public static int Add(String numbers) {
-        int i, length, sum=0;
-        if(!numbers.equals("")) {
-            String[] input = numbers.split("[, | \n]", 0);
+        int i, length, strLen, sum=0;
+        char delimeter = ',';
+        strLen = numbers.length();
+        if(strLen > 0) {
+            if((strLen > 2) && (numbers.substring(0, 2).equals("//"))) {
+                delimeter = numbers.charAt(2);
+                numbers = numbers.substring(4, numbers.length());
+            }
+            String[] input = numbers.split("[" + delimeter + " | \n]", 0);
             length = input.length;
             for(i=0; i<length; i++) {
                 sum += Integer.parseInt(input[i]);
--- test/java/CalculatorTest.java
+++ test/java/CalculatorTest.java
@@ -44,4 +44,16 @@ public class CalculatorTest {
         res = cal.Add("1,2\n3,4");
         assertTrue(res == 10);
     }
+
+    @Test
+    public void testAddCase4() {
+        Calculator cal = new Calculator();
+        int res;
+
+        res = cal.Add("//;\n1;2\n3");
+        assertTrue(res == 6);
+
+        res = cal.Add("//#\n1#2#3#4");
+        assertTrue(res == 10);
+    }
 }
